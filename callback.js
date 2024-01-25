fd1d4fd05b7946f637df37c286bf5ef70fa767f3
rahulredde01 <rahulreddymothe97512@gmail.com>
2024-01-25 10:20:42 +0530

    initial commit

---
+++ callback.js
@@ -0,0 +1,33 @@
+/*function parentFunction(name, callback){
+    callback();
+    console.log("Hey "+name);
+}
+
+function randomFunction(){
+    console.log("Hey I am callbackfunction");
+}
+parentFunction("Random String",randomFunction)
+function parentFunction(name, callback){
+    setTimeout(callback,1000);
+    console.log("Hey "+name);
+}
+function randomFunction(){
+    console.log("Hey I am callbackfunction");
+}
+parentFunction("Random String",randomFunction);
+
+function parentFunction(name, callback){
+    setTimeout(callback,1000);
+    console.log("Hey "+name);
+}
+parentFunction("Random string",function(){
+    console.log("Hey I am a callback function");
+});*/
+
+const parentFunction(name, callback){
+    setTimeout(callback,1000);
+    console.log("Hey "+name);
+}
+parentFunction("Random string",function(){
+    console.log("Hey I am a callback function");
+});
